921ec022d225e2b3ff09787d5d49e4ec98c8dde0
Ziaul Hoque <76832971+ziaulhoque24@users.noreply.github.com>
2022-11-24 13:06:25 +0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import Card from "./Card";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Card />
+    </div>
+  );
+}
---
+++ src/Card.jsx
@@ -0,0 +1,22 @@
+export default () => (
+  <div className="birthdayCard">
+    <div className="cardFront">
+      <h3 className="happy">HAPPY BIRTHDAY Love!</h3>
+      <div className="balloons">
+        <div className="balloonOne" />
+        <div className="balloonTwo" />
+        <div className="balloonThree" />
+        <div className="balloonFour" />
+      </div>
+    </div>
+    <div className="cardInside">
+      <h3 className="back">HAPPY BIRTHDAY vodoo!</h3>
+      <p>Dear Friend,</p>
+      <p>
+        Happy birthday!! I hope your day is filled with lots of love and
+        laughter! May all of your birthday wishes come true.
+      </p>
+      <p className="name">Pawan</p>
+    </div>
+  </div>
+);
